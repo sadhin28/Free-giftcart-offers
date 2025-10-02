8f7990c90b01b337af3825a9afedb5fdde42b742
sadhin28 <taosifsadhin113@gmail.com>
2025-10-02 16:15:23 +0600

    all done

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
   build: {
-    outDir: 'build'
+    outDir: 'dist' // must match Vercel’s output directory
   }
 })
