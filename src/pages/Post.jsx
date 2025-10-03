ab16f7af5dcbf8acdce7cedd6b7cd9a3388ae414
sadhin28 <taosifsadhin113@gmail.com>
2025-10-04 02:26:42 +0600

    add content locker link

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { posts } from '../posts'
+
 import { Helmet } from 'react-helmet'
 
 export default function Post(){
   const { slug } = useParams()
   const post = posts.find(p => p.slug === slug)
- const LOCKER_URL = 'https://d1y0yks1k8t5m5.cloudfront.net/c97a60b.js';
 
   if(!post) return <h2>Post not found</h2>
-
+  const OFFER_LINK=import.meta.env.VITE_OFFER_LINK;
   return (
     <article className="post">
       <Helmet>
@@ -20,7 +20,7 @@ export default function Post(){
       <h1>{post.title}</h1>
       <p>{post.content}</p>
 
-      <a href={LOCKER_URL} target="_blank" rel="noopener noreferrer" className="btn-claim">Claim Offer</a>
+      <a href={OFFER_LINK} target="_blank" rel="noopener noreferrer" className="btn-claim">Claim Offer</a>
     </article>
   )
 }
