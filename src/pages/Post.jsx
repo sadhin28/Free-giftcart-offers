45f723c5f0918a5f74fc825dff1a0341ec4fc440
sadhin28 <taosifsadhin113@gmail.com>
2025-10-04 01:43:47 +0600

    add offer link

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { posts } from '../posts'
-import { OFFER_LINK } from '../config'
 import { Helmet } from 'react-helmet'
 
 export default function Post(){
   const { slug } = useParams()
   const post = posts.find(p => p.slug === slug)
+ const LOCKER_URL = 'https://d1y0yks1k8t5m5.cloudfront.net/c97a60b.js';
 
   if(!post) return <h2>Post not found</h2>
 
@@ -20,7 +20,7 @@ export default function Post(){
       <h1>{post.title}</h1>
       <p>{post.content}</p>
 
-      <a href={OFFER_LINK} target="_blank" rel="noopener noreferrer" className="btn-claim">Claim Offer</a>
+      <a href={LOCKER_URL} target="_blank" rel="noopener noreferrer" className="btn-claim">Claim Offer</a>
     </article>
   )
 }
